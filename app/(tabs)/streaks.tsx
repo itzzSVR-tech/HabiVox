dac8e8cfbcf61c89804295b80f92f10e2405b1bb
itzzSVR-tech <marioplayz1975@gmail.com>
2025-08-11 07:09:22 +0530

    Basic Screens implemented

---
+++ app/(tabs)/streaks.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function StreaksScreen() {
+    return (
+        <View>
+            <Text>StreaksScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
---
+++ app/auth.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AuthScreen() {
+    return (
+        <View>
+            <Text>AuthScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
